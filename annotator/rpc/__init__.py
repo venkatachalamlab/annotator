67b4d458dcfc284a0805eb33393f7e5284ff0b34
mtorkashvand <m.torkashvand@northeastern.edu>
2021-02-19 10:28:38 -0500

    Add change_provenance
    A new macro to change the provenance for different time points and worldlines to a new value

--- annotator/rpc/__init__.py
+++ annotator/rpc/__init__.py
@@ -6,9 +6,10 @@ from .insert_annotation import insert_annotation
 from .worldline_utilities import randomly_color_worldlines, renumber_worldlines
 from .navigation import jump_to_frame
 from .create_track import create_track
+from .change_provenance import change_provenance
 
 __all__ = [
     "delete_annotations", "insert_local_max", "insert_annotation",
     "randomly_color_worldlines", "renumber_worldlines", "jump_to_frame",
-    "create_track"
+    "create_track", "change_provenance"
 ]
